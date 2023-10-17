46cdae7012636ac66bb6e7bde60083ae129be260
harolpg17 <harolpg17@gmail.com>
2023-10-17 14:56:22 -0500

    Leer argumentos por URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,18 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to="/marvel" />
+  }
+
   return (
     <>
-        <h1>Hero Page</h1>
+        <h1>{hero.superhero}</h1>
     </>
   )
 }
